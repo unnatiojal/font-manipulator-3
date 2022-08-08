5d6fe54daad7bffa7f35829ae9747ee5bc9c35bf
unnatiojal <85604066+unnatiojal@users.noreply.github.com>
2022-08-08 21:25:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(650, 150);
+
+    poseNet = ml5.poseNet(video, modalLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modalLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
+
+function draw() {
+    background('#969A97');
+    document.getElementById("square_side").innerHTML = "Font size of the text will be = "+ difference + "px";
+    textSize(difference);
+    fill('#e6e6fa');
+    text('Unnati ojal', 50, 400);
+}
